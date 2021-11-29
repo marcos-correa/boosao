3d44d3f6fa0e9737e5977195d890d7821349b4b9
marcos <marcoscorreats@gmail.com>
2021-11-29 11:18:15 -0300

    fix: express js to deploy on heroku

--- server.js
+++ server.js
@@ -4,24 +4,12 @@ const path = require('path');
 
 const app = express();
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/boosao'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/boosao/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/boosao/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
