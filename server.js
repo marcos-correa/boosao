d830504476486768d0a87bee9b37c4216fe39002
marcos <marcoscorreats@gmail.com>
2021-11-29 11:07:50 -0300

    fix: alterando o express

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/boosao'));
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/boosao/'}),
-);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
